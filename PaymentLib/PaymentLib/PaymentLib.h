557f9e07e21975dc0057d5c30af99fa2ccf19984
lei zhang <eirstone@gmail.com>
2022-10-01 13:08:28 +0100

    inital draft

---
+++ PaymentLib/PaymentLib/PaymentLib.h
@@ -0,0 +1,18 @@
+//
+//  PaymentLib.h
+//  PaymentLib
+//
+//  Created by Lei Zhang on 01/10/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PaymentLib.
+FOUNDATION_EXPORT double PaymentLibVersionNumber;
+
+//! Project version string for PaymentLib.
+FOUNDATION_EXPORT const unsigned char PaymentLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PaymentLib/PublicHeader.h>
+
+
---
+++ PaymentLib/PayrocExample/PayrocExampleApp.swift
@@ -0,0 +1,20 @@
+//
+//  PayrocExampleApp.swift
+//  PayrocExample
+//
+//  Created by Lei Zhang on 01/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct PayrocExampleApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
